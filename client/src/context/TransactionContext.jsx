a6e1515f42fac21815e5eb291b9d8dec453d3555
Mags <msmfsd@users.noreply.github.com>
2023-08-07 03:05:26 +1000

    dfgfgd

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,1 +1,42 @@
 import React, { useEffect, useContext } from "react";
+import { ethers } from "ethers";
+
+import { contractABI, contractAddress } from "../utils/constants";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.provider.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionsContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+
+  console.log({
+    provider,
+    signer,
+    transactionsContract,
+  });
+};
+
+export const TransactionProvider = ({ children }) => {
+  const checkIfWalletIsConnected = async () => {
+    if (!ethereum) return alert("Please install Metamask extension");
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <TransactionContext.Provider value={{ value: "test" }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+};
